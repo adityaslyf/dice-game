69fcc3cac8857dbfe1ab641bcf130a4d97b03271
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-05-13 23:50:39 +0530

    modify html

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-let randomNumber1 = Math.floor(Math.random()*6)+1;
-console.log(randomNumber1);
-function randomNumber1(){
-    document.getElementById("hue").setAttribute("src",+ randomNumber1 + "images/dice6.png");
-}
+// let randomNumber1 = Math.floor(Math.random()*6)+1;
+// console.log(randomNumber1);
+// function randomNumber1(){
+//     document.getElementById("hue").setAttribute("src",+ randomNumber1 + "images/dice6.png");
+// }
+
+// Generate a random number between 1 and 6
+// 
+
+var randomNumber = Math.floor(Math.random()*6)+1;
+var randomDiceImage = "player" +randomNumber + ".png";
+var randomImageSource ="images/" + randomDiceImage;
+var image1 = document.querySelectorAll("image")[0];
+image1.setAttribute("src",randomImageSource);
